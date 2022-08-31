3f88918de3292c241a3ae5c44bab1d8b14f41aed
fedegblack <74991240+fedegblack@users.noreply.github.com>
2022-08-31 16:53:36 -0300

    Update keymap.c

--- keyboards/redragon/k556/keymaps/default/keymap.c
+++ keyboards/redragon/k556/keymaps/default/keymap.c
@@ -220,7 +220,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
         return true;
-    */    case RESET_KB:
+          case RESET_KB:
             NVIC_SystemReset();
     }
     return true;
